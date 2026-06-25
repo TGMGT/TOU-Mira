bf7a3bed06bca2e83dd60a86b61acdf0a5a3c168
TGMGT <poop12689@gmail.com>
2026-06-26 00:20:23 +0400

    Implement Harmony patches for PassiveButton safety
    Added Harmony patches to prevent crashes when PassiveButton is null or has an invalid pointer.

---
+++ TownOfUs/Patches/Testfixstb.cs
@@ -0,0 +1,30 @@
+using System;
+using HarmonyLib;
+
+namespace YourExistingPluginNamespace
+{
+    public static class VanillaSettingsCrashPatch
+    {
+        [HarmonyPatch(typeof(PassiveButton), nameof(PassiveButton.ReceiveClickDown))]
+        [HarmonyPrefix]
+        public static bool PrefixClickDown(PassiveButton __instance)
+        {
+            if (__instance == null || __instance.Pointer == IntPtr.Zero)
+            {
+                return false;
+            }
+            return true;
+        }
+      
+        [HarmonyPatch(typeof(PassiveButton), nameof(PassiveButton.ReceiveClickUp))]
+        [HarmonyPrefix]
+        public static bool PrefixClickUp(PassiveButton __instance)
+        {
+            if (__instance == null || __instance.Pointer == IntPtr.Zero)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
